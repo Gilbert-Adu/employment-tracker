65fb355cc9a96f29f2687e94e0734d75297f931c
Gilbert Adu <gea33@cornell.edu>
2022-06-12 01:31:12 -0400

    transferred code

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
+
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on("error", (error) => console.error(error))
+db.once("open", () => console.log("connected to db!"))
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+});
+
+app.listen(port, () => {
+    console.log("server listening")
+});
